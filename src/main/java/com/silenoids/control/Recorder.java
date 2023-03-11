de71f45fedb9e020264dcb2046aa53b3188243b2
Sileno <city17@hotmail.it>
2023-03-11 12:09:06 +0100

    refactoring recorder

--- src/main/java/com/silenoids/control/Recorder.java
+++ src/main/java/com/silenoids/control/Recorder.java
@@ -10,10 +10,12 @@ import java.util.concurrent.CyclicBarrier;
 
 public class Recorder {
 
-    private AudioFormat outputLineFormat;
+    private final AudioFormat outputLineFormat;
     private TargetDataLine micLine;
 
     private CyclicBarrier synchronizedGate;
+    private Thread stopperThread;
+    private Thread writerThread;
 
     public Recorder() {
         outputLineFormat = new AudioFormat(
@@ -24,58 +26,71 @@ public class Recorder {
                 true
         );
 
+        try {
+            setUp();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
         synchronizedGate = new CyclicBarrier(2);
     }
 
     public void recordAudio(String dirPath, String fileName, long millisToRecord) {
-        try {
-            File targetOutputFile = FileUtils.loadDirFile(dirPath, fileName);
-            DataLine.Info outputLineInfo = new DataLine.Info(TargetDataLine.class, outputLineFormat);
-            micLine = (TargetDataLine) AudioSystem.getLine(outputLineInfo);
-            if (!AudioSystem.isLineSupported(outputLineInfo)) {
-                System.out.println("Line not supported");
-                return;
-            }
+        File targetOutputFile = FileUtils.loadDirFile(dirPath, fileName);
+        AudioInputStream audioInputStream = new AudioInputStream(micLine);
 
-            micLine.open(outputLineFormat, micLine.getBufferSize());
+        planToStopRecording(millisToRecord);
+        recordAudio(audioInputStream, targetOutputFile);
 
-            AudioInputStream audioInputStream = new AudioInputStream(micLine);
-
-            planToStopRecording(millisToRecord);
-            recordAudio(audioInputStream, targetOutputFile);
-
-            Thread.getAllStackTraces().keySet().stream().map(Thread::getName).sorted().forEach(System.out::println);
-
-        } catch (LineUnavailableException ex) {
-            ex.printStackTrace();
-        }
+        System.out.println("---Running thread list:");
+        Thread.getAllStackTraces().keySet().stream().map(Thread::getName).filter(s -> s.startsWith(" ")).sorted().forEach(System.out::println);
 
         Sandglass.getInstance().startSandglass(millisToRecord);
     }
 
+    private void setUp() throws LineUnavailableException {
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, outputLineFormat);
+        if (!AudioSystem.isLineSupported(info)) {
+            throw new LineUnavailableException("No line supported for the DataLine Info");
+        }
+        micLine = (TargetDataLine) AudioSystem.getLine(info);
+        micLine.open(outputLineFormat, micLine.getBufferSize());
+    }
+
     private void planToStopRecording(long millis) {
-        new Thread(() -> {
-            try {
-                synchronizedGate.await();
-                micLine.start();
-                Thread.sleep(millis);
-            } catch (InterruptedException | BrokenBarrierException ex) {
-                ex.printStackTrace();
-            }
-            micLine.stop();
-            micLine.close();
-        }, " Recording stopper").start();
+        if (stopperThread == null) {
+            stopperThread = new Thread(() -> {
+                System.out.println("started stopper per millis: " + millis);
+                try {
+                    synchronizedGate.await();
+                    micLine.start();
+                    Thread.sleep(millis);
+                } catch (InterruptedException | BrokenBarrierException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    micLine.stop();
+                }
+            }, " Recording stopper");
+        }
+
+        stopperThread.start();
     }
 
     private void recordAudio(AudioInputStream audioInputStream, File targetOutputFile) {
-        new Thread(() -> {
-            try {
-                synchronizedGate.await();
-                AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, targetOutputFile);
-            } catch (InterruptedException | BrokenBarrierException | IOException ex) {
-                ex.printStackTrace();
-            }
-        }, " Recording writing").start();
+        if (writerThread == null) {
+            writerThread = new Thread(() -> {
+                System.out.println("started writer su output " + targetOutputFile);
+
+                try {
+                    synchronizedGate.await();
+                    AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, targetOutputFile);
+                } catch (InterruptedException | BrokenBarrierException | IOException ex) {
+                    ex.printStackTrace();
+                }
+            }, " Recording writing");
+        }
+
+        writerThread.start();
     }
 
 }
